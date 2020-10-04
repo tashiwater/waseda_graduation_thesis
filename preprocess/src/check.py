3823653065c1556c12b0dfce27dd2265a451ad00
tashiwater <tashiwater@gmail.com>
2020-10-04 15:57:58 +0900

    add check of wiping data

---
+++ preprocess/src/check.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# coding: utf-8
+import os
+from pathlib import Path
+import pandas as pd
+import numpy as np
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = CURRENT_DIR + "/../data/"
+dir_path = "/home/user/TAKUMI_SHIMIZU/wiping/data/1002/tactile_raw/"
+paths = [str(p) for p in Path(dir_path).glob("./*.csv")]
+paths.sort()
+
+for i, path in enumerate(paths):
+    df = pd.read_csv(path)
+
+    tail = df.iloc[-1]
+    max_val = tail.max()
+    if max_val > 0.1:
+        all_max = df.max(axis=0).max()
+        print(i, max_val, all_max)
+
