c9b31e45fe742fd9bc685ed615cf1a631cadf686
tashiwater <tashiwater@gmail.com>
2020-10-06 18:06:07 +0900

    a

--- MTRNN/src/train_node.py
+++ MTRNN/src/train_node.py
@@ -10,6 +10,7 @@ import torch
 name = input("file name :")
 cf_num = int(input("cf_num (default = 200):"))
 cs_tau = int(input("cs_tau (default = 50):"))
+load_path = input("?.pth:")
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = CURRENT_DIR + "/../data/"
 TRAIN_PATH = DATA_DIR + "train"
@@ -26,7 +27,7 @@ in_size = trainset[0][0].shape[1]
 net = MTRNN(
     layer_size={"in": in_size, "out": in_size, "io": 34, "cf": cf_num, "cs": 15},
     tau={"tau_io": 2, "tau_cf": 5, "tau_cs": cs_tau},
-    open_rate=0.8,
+    open_rate=0.3,
 )
 ## modelをロードしたとき
 # model_path = MODEL_DIR + "20200612_103406_100.pth"
@@ -52,5 +53,6 @@ train_net = TrainNet(
     MODEL_DIR,
     param_dict,
 )
-train_net.load_model(input("?.pth:"))
+if load_path != "":
+    train_net.load_model(load_path)
 train_net.run()
