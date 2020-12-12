17de2b0e912fdf44c3ca539bdaa5ad8b17668148
tashiwater <tashiwater@gmail.com>
2020-12-12 20:51:15 +0900

    90 10 check

--- NN/src/test/test_MTRNN_size.py
+++ NN/src/test/test_MTRNN_size.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     open_rate = 0.1
 
     CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-    DATA_DIR = CURRENT_DIR + "/../../data/MTRNN/1210/noimg/"
+    DATA_DIR = CURRENT_DIR + "/../../data/MTRNN/1210/size/"
     TEST_PATH = DATA_DIR + "train"
     RESULT_DIR = DATA_DIR + "result/"
     MODEL_BASE = "/media/user/ボリューム/model/"
@@ -31,10 +31,10 @@ if __name__ == "__main__":
     #     int(open_rate * 10), name
     # )
     MODEL_DIR = MODEL_BASE + "MTRNN/"
-    load_path = "1210/noimg/cf10/3000/{}_{}".format(cf_num, cs_num)
+    load_path = "1210/size/open_01/1000/{}_{}".format(cf_num, cs_num)
     # load_path = "1119_70_8/20201120_001102_10000finish"
     dataset = MyDataSet(TEST_PATH)
-    in_size, out_size = 30, 30  # trainset[0][0].shape[1]
+    in_size, out_size = 30, 32  # trainset[0][0].shape[1]
     position_dims = 7
     net = Net(
         layer_size={
@@ -60,7 +60,7 @@ if __name__ == "__main__":
             + [add_word + "torque{}".format(i) for i in range(7)]
             + [add_word + "tactile{}".format(i) for i in range(16)]
             # + [add_word + "image{}".format(i) for i in range(15)]
-            # + [add_word + "size{}".format(i) for i in range(2)]
+            + [add_word + "size{}".format(i) for i in range(2)]
         )
 
     dataloader = torch.utils.data.DataLoader(
